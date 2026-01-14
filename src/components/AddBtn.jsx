4139c1c32ef0bd62e9e0766b0e0cb20ad9883c33
bomdong628 <jae0ee12@gmail.com>
2026-01-14 16:04:24 +0900

    feat: 지출 내역 추가버튼 컴포넌트 추가

---
+++ src/components/AddBtn.jsx
@@ -0,0 +1,15 @@
+import { Plus } from 'lucide-react';
+import { COLORS } from '../constants';
+
+function AddBtn({ setIsFormOpen }) {
+  return (
+    <button
+      onClick={() => setIsFormOpen(true)}
+      className={`fixed bottom-24 right-4 w-14 h-14 ${COLORS.accentBg} rounded-full shadow-lg flex items-center justify-center text-white hover:scale-105 transition-transform z-50`}
+    >
+      <Plus size={28} />
+    </button>
+  );
+}
+
+export default AddBtn;
