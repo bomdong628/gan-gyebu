bbeb7e6c851e46d3975751963477c953da1f943a
bomdong628 <jae0ee12@gmail.com>
2026-01-14 15:58:25 +0900

    feat: 상수 선언 및 util 함수 추가

---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+/** 기본 카테고리 정의 */
+export const DEFAULT_CATEGORIES = ['식비', '교통', '생활', '쇼핑', '의료', '주거', '통신', '기타'];
+/** 지출자 정의 */
+export const PAYERS = ['남편', '아내', '공동'];
+/** 지출수단 정의 */
+export const METHODS = ['카드', '현금', '계좌'];
+
+/** 색상 정의 */
+export const COLORS = {
+  bg: 'bg-[#FEFDF5]', // Off-white/Eggshell
+  primary: 'bg-[#F4E06D]', // Yolk Yellow
+  primaryHover: 'hover:bg-[#EED550]',
+  secondary: 'bg-[#F0E6D2]', // Beige
+  accent: 'text-[#6B4E38]', // Soy Sauce Brown
+  accentBg: 'bg-[#6B4E38]',
+  text: 'text-[#5A4635]',
+  border: 'border-[#E0D0B8]',
+  white: 'bg-white',
+  danger: 'text-red-400',
+  chart: ['#F4E06D', '#E6C288', '#D6B470', '#C6A658', '#B69840', '#A68A28', '#8D7B68', '#A4907C'],
+};
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export const formatMoney = (val) => new Intl.NumberFormat('ko-KR').format(val) + '원';
